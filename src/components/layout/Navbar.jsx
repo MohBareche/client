33256f652a00865e14338241255c1fddabf94461
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2023-10-30 06:43:49 -0400

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -9,10 +9,8 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
-                <img src={logo} width="24" height="24" />
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
+                <img src={logo} width="36" height="36" />
+                MohApp
             <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
                 <li className="nav-item">
