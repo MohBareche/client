069a5e77ddb9523a44187e130d565f23f4e1e84c
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2023-10-30 06:10:01 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,8 +43,9 @@ function App() {
                         )}
                     </Routes>
                 </div>
+                <ToastContainer />
             </BrowserRouter>
-            <ToastContainer />
+            
         </>
     );
 }
